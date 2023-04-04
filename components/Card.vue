e8c154c14d00f120010cc1343c57debbcf4ec360
Sridharvn <sridharvnampoothiripad@gmail.com>
2023-04-04 19:48:29 +0530

    Created basic starting page

---
+++ components/Card.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="flex justify-center items-center border mx-96 text-center ">
+
+        <slot></slot>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,4 @@
 <template>
     <div class="font-bold flex text-center justify-center items-center p-10 text-3xl">Welcome To the Game</div>
+    <Card>Hello</Card>
 </template>

21b5ea497ec431f679509e6451be4e9c87a9dafe
Sridharvn <sridharvnampoothiripad@gmail.com>
2023-04-06 21:25:03 +0530

    Added before beginning screen

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex justify-between items-center">
         <img src="./../assets/images/logo-social.png" alt="Logo" class="h-24 mix-blend-multiply">
-        <ul class="flex justify-end mr-96 ">
+        <ul class="flex justify-end mr-10 md:mr-64 sm:mr-32 lg:mr-96">
             <li class="p-2">1</li>
             <li class="p-2">2</li>
             <li class="p-2">3</li>
--- pages/index.vue
+++ pages/index.vue
@@ -2,24 +2,87 @@
     <div class="font-bold flex text-center justify-center items-center p-10 text-4xl text-blue-600">
         Welcome To the Game
     </div>
-    <Card>
-        <div v-if="questions.length == 0">Loading...</div>
-        <div v-else>
-            <h1>
-                {{ questions[0].category }}
-            </h1>
-            <br />
-            <p>
-                {{ questions[0].question }}
+    <div v-if="isStarted">
+        <Card>
+            <div v-if="questions.length == 0">Loading...</div>
+            <div v-else>
+                <h1>
+                    {{ questions[0].category }}
+                </h1>
                 <br />
-                True or False?
-            </p>
+                <p>
+                    {{ questions[0].question }}
+                    <br />
+                    True or False?
+                </p>
+            </div>
+        </Card>
+    </div>
+    <div v-else>
+        <div class="lg:text-xl text-center lg:mx-64 md:mx-32 sm:mx-10 mx-4">This is a true or false game. You wil be given a
+            statement
+            that might
+            be
+            true
+            or
+            false. You have to select the right choice and move to the next questions. You will be shown your final score
+            after the game, from where you can reset the game.</div>
+        <div class="flex justify-center items-center mt-10">
+            <table>
+                <tr>
+                    <td>
+                        <label for="countries"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Number of
+                            Questions : &nbsp;</label>
+                    </td>
+                    <td class="flex justify-end">
+                        <select id="countries"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            v-model="amount">
+                            <option selected value="10">10</option>
+                            <option value="20">20</option>
+                            <option value="30">30</option>
+                            <option value="40">40</option>
+                            <option value="50">50</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="countries"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Difficulty :
+                            &nbsp;</label>
+                    </td>
+                    <td class="flex justify-end">
+                        <select id="countries"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            v-model="difficulty">
+                            <option selected value="Easy">Easy</option>
+                            <option value="Medium">Medium</option>
+                            <option value="Hard">Hard</option>
+                        </select>
+                    </td>
+                </tr>
+            </table>
         </div>
-    </Card>
+        <div class="flex justify-center mt-10">
+            <button type="button"
+                class="text-white text-xl ml-10 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Begin</button>
+        </div>
+    </div>
 </template>
+<style  scoped>
+* {
+    /* border: 1px solid black; */
+}
 
+td {
+    padding: 5px;
+}
+</style>
 <script>
 import axios from "axios";
+
 export default {
     methods: {
         async getQuestions(amount, difficulty) {
@@ -43,6 +106,9 @@ export default {
         return {
             API_URL: "https://opentdb.com/api.php",
             questions: [],
+            isStarted: false,
+            amount: 10,
+            difficulty: "Easy"
         };
     },
     mounted() {

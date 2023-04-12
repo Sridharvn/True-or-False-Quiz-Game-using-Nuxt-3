094d620382778a6e017a9a900befeebf36e1b259
Sridharvn <sridharvnampoothiripad@gmail.com>
2023-04-12 21:24:17 +0530

    Added back output and nuxt-build

---
+++ nuxt-build/prerender/index.mjs
@@ -0,0 +1,15 @@
+globalThis._importMeta_={url:import.meta.url,env:process.env};import 'file://D:/Personal-Projects/True-False-Game/quiz-game/node_modules/node-fetch-native/dist/polyfill.mjs';
+export { l as localFetch } from './chunks/nitro-prerenderer.mjs';
+import 'file://D:/Personal-Projects/True-False-Game/quiz-game/node_modules/h3/dist/index.mjs';
+import 'file://D:/Personal-Projects/True-False-Game/quiz-game/node_modules/ofetch/dist/node.mjs';
+import 'file://D:/Personal-Projects/True-False-Game/quiz-game/node_modules/destr/dist/index.mjs';
+import 'file://D:/Personal-Projects/True-False-Game/quiz-game/node_modules/unenv/runtime/fetch/index.mjs';
+import 'file://D:/Personal-Projects/True-False-Game/quiz-game/node_modules/hookable/dist/index.mjs';
+import 'file://D:/Personal-Projects/True-False-Game/quiz-game/node_modules/scule/dist/index.mjs';
+import 'file://D:/Personal-Projects/True-False-Game/quiz-game/node_modules/defu/dist/defu.mjs';
+import 'file://D:/Personal-Projects/True-False-Game/quiz-game/node_modules/ohash/dist/index.mjs';
+import 'file://D:/Personal-Projects/True-False-Game/quiz-game/node_modules/ufo/dist/index.mjs';
+import 'file://D:/Personal-Projects/True-False-Game/quiz-game/node_modules/unstorage/dist/index.mjs';
+import 'file://D:/Personal-Projects/True-False-Game/quiz-game/node_modules/unstorage/drivers/fs.mjs';
+import 'file://D:/Personal-Projects/True-False-Game/quiz-game/node_modules/radix3/dist/index.mjs';
+//# sourceMappingURL=index.mjs.map

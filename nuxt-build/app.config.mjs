ea57ecf160a2aa2a082bf70dfa6c3ea3ec108735
Sridharvn <sridharvnampoothiripad@gmail.com>
2023-04-12 21:34:24 +0530

    Added back nuxt-build and output

---
+++ nuxt-build/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from 'D:/Personal-Projects/True-False-Game/quiz-game/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default /* #__PURE__ */ defuFn(inlineConfig)

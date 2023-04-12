dc7f0879d61f95b0ff8398413c5719b0274c93fd
Sridharvn <sridharvnampoothiripad@gmail.com>
2023-04-12 21:18:56 +0530

    Removed output and nuxt-build

--- nuxt-build/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from 'D:/Personal-Projects/True-False-Game/quiz-game/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default /* #__PURE__ */ defuFn(inlineConfig)
--- nuxt-build/prerender/chunks/index-styles.635e2d51.mjs
+++
@@ -1,6 +0,0 @@
-const index_vue_vue_type_style_index_0_scoped_cf577e27_lang = "td[data-v-cf577e27]{padding:5px}";
-
-const indexStyles_635e2d51 = [index_vue_vue_type_style_index_0_scoped_cf577e27_lang];
-
-export { indexStyles_635e2d51 as default };
-//# sourceMappingURL=index-styles.635e2d51.mjs.map
